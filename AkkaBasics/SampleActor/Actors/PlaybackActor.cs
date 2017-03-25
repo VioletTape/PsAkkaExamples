3674edf0e6f8d70aca8475eeb504c2eb8c3aecdd
Violet Tape <my@violet-tape.net>
2017-03-25 10:57:25 +0100

    add hocon and first actor

---
+++ AkkaBasics/SampleActor/Actors/PlaybackActor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Akka.Actor;
+
+namespace SampleActor.Actors {
+    public class PlaybackActor : UntypedActor {
+        public PlaybackActor() {
+            Console.WriteLine("Hello from playback actor!");
+        }
+
+        protected override void OnReceive(object message) {
+            
+        }
+    }
+}
--- AkkaBasics/SampleActor/Program.cs
+++ AkkaBasics/SampleActor/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Akka.Actor;
+using SampleActor.Actors;
 
-namespace SampleActor
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace SampleActor {
+    internal class Program {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        private static void Main(string[] args) {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingSystem");
+            var ms = MovieStreamingActorSystem;
+            Console.WriteLine("Actor system was created");
+
+            var props = Props.Create<PlaybackActor>();
+            var actorRef = ms.ActorOf(props, "PlaybackActor");
+
+            Console.ReadLine();
+
+            ms.Terminate();
+
         }
     }
 }
