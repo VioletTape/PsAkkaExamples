d160de7754996794991191e3364cef737cc2efeb
Violet Tape <my@violet-tape.net>
2017-03-30 15:54:35 +0200

    Add commands Play, Stop, Exit and process them

--- AkkaBasics/SimpleHierarchyActors/Program.cs
+++ AkkaBasics/SimpleHierarchyActors/Program.cs
@@ -3,22 +3,38 @@ using Akka.Actor;
 using NLog;
 using NLog.Config;
 using SimpleHierarchyActors.Actors;
+using SimpleHierarchyActors.Messages;
 
 namespace SimpleHierarchyActors {
     internal class Program {
         private static void Main(string[] args) {
             LogManager.Configuration = new XmlLoggingConfiguration("NLog.config", true);
 
-            var asMoviewStore = ActorSystem.Create("MovieStore");
+            var movieStore = ActorSystem.Create("MovieStore");
+            movieStore.ActorOf<PlaybackActor>("playback");
 
-            var playbackActor = asMoviewStore.ActorOf<PlaybackActor>("playback");
+            Console.WriteLine("Available commands:\n\tplay -<userId> -<movie name>\n\tstop -<userId>\n\texit");
 
-            Console.ReadLine();
+            do {
+                var command = Console.ReadLine();
+                var myArgs = command.Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
 
-            asMoviewStore.Terminate();
+                if (command.StartsWith("play")) {
+                    movieStore.ActorSelection("/user/playback/userCoordinator")
+                        .Tell(new PlayMovieMessage(int.Parse(myArgs[1]), myArgs[2]));
+                }
+                if (command.StartsWith("stop")) {
+                    movieStore.ActorSelection("/user/playback/userCoordinator")
+                              .Tell(new StopMovieMessage(int.Parse(myArgs[1])));
+                }
+                if (command.StartsWith("exit")) {
+                    var task = movieStore.Terminate();
+                    Console.WriteLine("Actor system terminating...");
+                    task.Wait();
+                    Environment.Exit(1);
+                }
 
-            Console.WriteLine("Press Enter to exit.");
-            Console.ReadLine();
+            } while (true);
         }
     }
 }
