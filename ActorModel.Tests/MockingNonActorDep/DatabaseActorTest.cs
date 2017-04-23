45575d295df6154bf0dee6aec722a6e676d00461
Violet Tape <my@violet-tape.net>
2017-04-23 10:21:43 +0200

    Add usage of TestProbe instead of handwritten mock actors

--- ActorModel.Tests/MockingNonActorDep/DatabaseActorTest.cs
+++ ActorModel.Tests/MockingNonActorDep/DatabaseActorTest.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using ActorModel.Actors;
 using ActorModel.Messages;
 using Akka.Actor;
+using Akka.TestKit;
 using Akka.TestKit.NUnit;
-using Akka.TestKit.TestActors;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -11,17 +12,16 @@ using NUnit.Framework;
 namespace ActorModel.Tests.MockingNonActorDep {
     [TestFixture]
     public class DatabaseActorTest : TestKit {
-
         /// <summary>
-        /// Simplest way of mocking. Pay attention on ActorOf method
+        ///     Simplest way of mocking. Pay attention on ActorOf method
         /// </summary>
         [Test]
         public void TraditionalMocking() {
             // arrange
             var stats = new Dictionary<string, int> {
-                                            {"Batman", 100}
-                                            , {"Batman Returns", 200}
-                                        };
+                                                        {"Batman", 100}
+                                                        , {"Batman Returns", 200}
+                                                    };
 
             var mock = new Mock<IDatabaseGateway>();
             mock.Setup(_ => _.GetStredStatistics())
@@ -40,11 +40,10 @@ namespace ActorModel.Tests.MockingNonActorDep {
 
 
         /// <summary>
-        /// using handwritten mock actor that works in system
+        ///     using handwritten mock actor that works in system
         /// </summary>
         [Test]
-        public void MockingActors()
-        {
+        public void MockingActors() {
             // arrange
             var mockDbActor = ActorOfAsTestActorRef<MockDatabaseActor>();
 
@@ -53,6 +52,37 @@ namespace ActorModel.Tests.MockingNonActorDep {
 
             actor.UnderlyingActor.PlayCounts["Batman"].Should().Be(100);
         }
+
+        /// <summary>
+        ///     now we are going to use test probe instead of handwritten actor
+        /// </summary>
+        [Test]
+        public void UsingTestProbe() {
+            // arrange
+            var mockDbActor = CreateTestProbe();
+            var autoPilot = new DelegateAutoPilot((sender, message) => {
+                                                  var stats = new Dictionary<string, int> {
+                                                                                              {"Batman", 100}
+                                                                                              , {"Batman Returns", 200}
+                                                                                          };
+                                                  sender.Tell(new InitialStatisticsMesage(new ReadOnlyDictionary<string, int>(stats)));
+                                                  return AutoPilot.KeepRunning;
+                                              });
+            mockDbActor.SetAutoPilot(autoPilot);
+
+            var actor = ActorOfAsTestActorRef(() => new StatisticsActor(mockDbActor));
+
+
+            actor.UnderlyingActor.PlayCounts["Batman"].Should().Be(100);
+        }
+
+        [Test]
+        public void ChekingCorrectMessageByTestProbe() {
+            var mockDbActor = CreateTestProbe();
+
+            ActorOf(() => new StatisticsActor(mockDbActor));
+
+            mockDbActor.ExpectMsg<GetInititalStatisticsMessage>();
+        }
     }
 }
-    
